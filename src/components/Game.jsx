867d3b14761aacbca31ab44b3e855f2a2308ba9f
Sonal Maheshwari <sonal.obedient@gmail.com>
2025-12-07 21:28:46 +0100

    Feature/game page (#6)
    * difficulty and start game function created
    
    -difficulty levels
    -start game function
    
    * changes in moves and time
    
    -flip card and new flipped cards matching and moves if not match the cards
    
    * function savestats for new start game
    
    -local storage
    -logout function and login function
    -navigate diff pages
    
    * updated

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,14 +2,24 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Game() {
+  const navigate = useNavigate();
   const [cards, setCards] = useState([]);
   const [flipped, setFlipped] = useState([]);
   const [matched, setMatched] = useState([]);
   const [moves, setMoves] = useState(0);
   const [time, setTime] = useState(0);
   const [difficulty, setDifficulty] = useState("easy");
+  const [timerActive, setTimerActive] = useState(false);
+  const [win, setWin] = useState(false);
 
 }
+
+const difficultyPairs = {
+  easy: 6,
+  medium: 8,
+  hard: 12,
+};
+
 
 // Timer //
 
@@ -51,7 +61,89 @@ setCards(shuffled);
 
 
 
-return (
+function startGame() {
+  setMoves(0);
+  setTime(0);
+  setFlipped([]);
+  setMatched([]);
+  setWin(false);
+  loadCards();
+  setTimerActive(true);
+}
+
+
+useEffect(() => {
+  startGame();
+}, [difficulty]);
+
+
+//flip card mapping//
+
+function flipCard(index) {
+  if (flipped.length === 2) return;
+  if (flipped.includes(index)) return;
+  if (matched.includes(cards[index].id)) return;
+
+  const newFlipped = [...flipped, index];
+  setFlipped(newFlipped);
+
+  if (newFlipped.length === 2) {
+    setMoves((m) => m + 1);
+
+    const [i1, i2] = newFlipped;
+    if (cards[i1].id === cards[i2].id) {
+      setMatched((prev) => [...prev, cards[i1].id]);
+
+      setFlipped([]);
+
+
+      if (matched.length + 1 === difficultyPairs[difficulty]) {
+        setTimerActive(false);
+        saveStats();
+        setWin(true);
+      }
+
+    } else {
+      setTimeout(() => setFlipped([]), 1000);
+    }
+  }
+}
+
+
+function saveStats() {
+  const stats = JSON.parse(localStorage.getItem("stats") || "[]");
+  stats.push({
+    user: loggedUser,
+    mode: difficulty,
+    moves,
+    time,
+    date: new Date().toLocaleString(),
+  });
+
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+
+function logout() {
+  localStorage.removeItem("loggedIn");
+  navigate("/login");
+}
+
+
+
+
+   return (
+    <div className="game-container">
+
+      <h2>Pokémon Memory Game</h2>
+
+    
+
+     //</div> <div style={{ marginTop: 10, textAlign: "center" }}>
+        <button onClick={() => setDifficulty("easy")}>Easy</button>
+        
+      </div>
+
+
   <div
     key={card.key}
     className={`card ${isFlipped ? "flipped" : ""}`}
