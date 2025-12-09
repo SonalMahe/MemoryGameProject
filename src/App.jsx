c0ec734b96354682cef96f247d0b2f872a959550
aripineninavya-rgb <aripineninavya@gmail.com>
2025-12-09 15:36:42 +0100

    first commint (#4)
    * first commint
    
    * added required functions and const variables in login page
    
    * small adjustments
    
    * created Navpage
    
    * updated
    
    ---------
    
    Co-authored-by: Sonal Maheshwari <sonal.obedient@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes,Route, Navigate} from 'react-router-dom'
+
+import Login from './Pages/login.jsx';
+import Register from './Pages/register.jsx';
+import Game from './Pages/game.jsx'
+import Navbar from './Components/Navbar.jsx';
+import Home from './Pages/Home.jsx';
+
+
+
 
 function App() {
   
 
   return (
-    <></>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element= { <Home />}/>
+        <Route path='/login' element= { <Login />}/>
+        <Route path='/register' element= { <Register />}/>
+        <Route path='/game' element= { <Game />}/>
+
+      </Routes>
+
+    </Router>
       
   )
 }
 
-export default App
+export default App;
