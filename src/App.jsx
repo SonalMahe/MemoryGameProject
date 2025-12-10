2b4c4662b5c79b751f9ef53355de8e9b47a0e23b
aripineninavya-rgb <aripineninavya@gmail.com>
2025-12-10 11:24:47 +0100

    adding css for login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes,Route, Navigate} from 'react-router-dom'
 
-import Login from './Pages/login.jsx';
-import Register from './Pages/register.jsx';
-import Game from './Pages/game.jsx'
-import Navbar from './Components/Navbar.jsx';
-import Home from './Pages/Home.jsx';
+import Login from './Components/login.jsx';
+import Register from './Components/register.jsx';
+import Game from './Components/Game.jsx';
+
+
 
 
 
@@ -15,9 +15,9 @@ function App() {
 
   return (
     <Router>
-      <Navbar/>
+      
       <Routes>
-        <Route path='/' element= { <Home />}/>
+        <Route path='/' element = {<Navigate to="/login"/>} />
         <Route path='/login' element= { <Login />}/>
         <Route path='/register' element= { <Register />}/>
         <Route path='/game' element= { <Game />}/>
